be4826bab9e7f350129d7ff5451021664d9fdd24
jamaludin.fiqri <jamaludin.fiqri@bukalapak.com>
2022-05-31 20:47:48 +0700

    add provider validation and display wallet user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
+  const [provider, setProvider] = useState();
+  const [address, setAddress] = useState();
+
+  useEffect(() => {
+    const initProvider = async () => {
+      const ethProvider = await detectEthereumProvider();
+      if (!ethProvider) {
+        alert('Metamask is not installed');
+        return;
+      }
+      const provider = new ethers.providers.Web3Provider(ethProvider);
+      setProvider(provider);
+    }
+
+    initProvider();
+  }, []);
+
+  const connectToWallet = async () => {
+    try {
+      if (!provider) {
+        throw new Error('Provider is not initialized');
+      }
+      await provider.send('eth_requestAccounts', []);
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      setAddress(address);
+    } catch (e) {
+      alert(e.message);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={connectToWallet}>
+          Connect to Metamask
+        </button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +52,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {
+            address && <span>Your address: {address}</span>
+          }
         </a>
       </header>
     </div>
