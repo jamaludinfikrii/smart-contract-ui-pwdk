8caea112f68d89694feeee4f47d919ee00582f1c
jamaludin.fiqri <jamaludin.fiqri@bukalapak.com>
2022-06-02 20:22:07 +0700

    test connect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 import { useWallet } from './context/WalletContext'
-
+import Abi from './abi/erc721Starter.json'
+import { ethers } from 'ethers'
+import { useEffect } from 'react';
 
 
 
 function App() {
 
   const {
+    provider,
     walletAddress,
     connectToWallet,
     isShowConnectMumbaiBtn,
     switchToMumbai,
     chainId
   } = useWallet();
+
+  useEffect(() => {
+
+    const testSmartContract = async () => {
+      const contractAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
+      const contract = new ethers.Contract(
+        contractAddress,
+        Abi.abi,
+        provider
+      )
+
+      const nftPrice = await contract.PRICE()
+      console.log(nftPrice.toString())
+      console.log(contract)
+    }
+
+    if(provider) {
+      testSmartContract()
+    }
+
+  }, [provider])
 
 
   return (
